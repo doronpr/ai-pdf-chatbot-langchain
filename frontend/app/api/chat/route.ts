725d7fe8bce9a32a51ddd31f5725320743463f63
Doron Pearl <doronpr@users.noreply.github.com>
2025-03-20 21:20:08 -0400

    f

--- frontend/app/api/chat/route.ts
+++ frontend/app/api/chat/route.ts
@@ -28,17 +28,17 @@ export async function POST(req: Request) {
       );
     }
 
-    if (!process.env.LANGGRAPH_RETRIEVAL_ASSISTANT_ID) {
-      return new NextResponse(
-        JSON.stringify({
-          error: 'LANGGRAPH_RETRIEVAL_ASSISTANT_ID is not set',
-        }),
-        { status: 500, headers: { 'Content-Type': 'application/json' } },
-      );
-    }
+    // if (!process.env.LANGGRAPH_RETRIEVAL_ASSISTANT_ID) {
+    //   return new NextResponse(
+    //     JSON.stringify({
+    //       error: 'LANGGRAPH_RETRIEVAL_ASSISTANT_ID is not set',
+    //     }),
+    //     { status: 500, headers: { 'Content-Type': 'application/json' } },
+    //   );
+    // }
 
     try {
-      const assistantId = process.env.LANGGRAPH_RETRIEVAL_ASSISTANT_ID;
+      const assistantId = 'retrieval_graph';
       const serverClient = createServerClient();
 
       const stream = await serverClient.client.runs.stream(
